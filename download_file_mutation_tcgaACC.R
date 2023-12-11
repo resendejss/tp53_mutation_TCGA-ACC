45f4763d629ac75760d0f86dde509b605e4c3b1a
Jean Silva de Souza Resende <jean.souza@edu.unipar.br>
2023-12-11 19:02:42 -0300

    update

---
+++ download_file_mutation_tcgaACC.R
@@ -0,0 +1,12 @@
+################################################################################
+## Name: download_file_mutation_tcgaACC.R                                     ##
+##                                                                            ##
+## Author: Jean Resende (jean.s.s.resende@gmail.com)                          ##
+## Date of last update: 12/11/2023                                            ##
+##                                                                            ##
+## Description: This script downloads the TCGA-ACC mutations available in the ##
+##              maf file                                                      ##
+################################################################################
+
+# -- Required packages --
+
